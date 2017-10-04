eabad6f7095410bafa84b3f11ab22d594baf34f0
mdchia <mdchia@users.noreply.github.com>
2017-10-05 01:23:03 +1100

    test update

--- vm_updates.sh
+++ vm_updates.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 # Updates to the standard class rep install system.
+R -e 'install.packages("tidyverse", repos="http://cran.csiro.au/")'
 
+echo('Updates complete!')
+echo('Press enter to finish')
+read
